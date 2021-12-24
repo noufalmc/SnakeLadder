9194558544f5eb89faa149b59f244e76e98f361e
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-24 11:04:24 +0530

    added:welcome message

---
+++ src/snakeladder/Main.java
@@ -0,0 +1,8 @@
+package snakeladder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//
+    }
+}
---
+++ src/snakeladder/SnakeLadder.java
@@ -0,0 +1,11 @@
+package snakeladder;
+
+public class SnakeLadder {
+    public static  final int IS_LADDER=1;
+    public static final int IS_SNAKE = 2;
+    public static final int IS_NO_PLAY=0;
+    public static void  main(String[] args)
+    {
+        System.out.println("Welcome to Snake Ladder");
+    }
+}
